4edc2ce7ed3166003958ccf2a3b75f605143068d
Dudeiebot <ojoexcel5@gmail.com>
2023-08-12 21:58:25 +0100

    ping

--- cmd/net/ping.go
+++ cmd/net/ping.go
@@ -5,31 +5,49 @@ package net
 
 import (
 	"fmt"
+	"net"
 	"net/http"
+	"time"
 
 	"github.com/spf13/cobra"
 )
 
-func ping (domain string) (int, error) {
-	
-}
-
-var (
+var ( //our variable from our flags adding, we add it here
 	urlPath string
+
+	client = http.Client{ //we are creating our client instead of using http.DefaultClient, which have all the timeout and shiit but we are customising our own here
+		Transport: &http.Transport{
+			Dial: (&net.Dialer{Timeout: 2 * time.Second}).Dial, //here is all our custom timeout and et all
+		},
+	}
 )
+
+func ping(domain string) (int, error) {
+	url := "http://" + domain
+	req, err := http.NewRequest("HEAD", url, nil)
+	if err != nil {
+		return 0, err
+	}
+	resp, err := client.Do(req)
+	if err != nil {
+		return 0, err
+	}
+	resp.Body.Close()
+	return resp.StatusCode, nil
+}
 
 // pingCmd represents the ping command
 var pingCmd = &cobra.Command{
 	Use:   "ping",
 	Short: "These pings network and returns a reponse",
-	Long: ``,
+	Long:  ``,
 	Run: func(cmd *cobra.Command, args []string) {
-	
+
 		//What we want it to do here
-		var client = &http.Client{
-			Transport: &http.Transport {
-				Dial: net.Dialer{timeout: 2 * time.Second}.Dial,
-			}
+		if resp, err := ping(urlPath); err != nil {
+			fmt.Println(err)
+		} else {
+			fmt.Println(resp)
 		}
 	},
 }
